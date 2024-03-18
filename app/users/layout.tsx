97625c52ac609eb9eb476d551d3a9d199966e554
Pratap Rathi <prataprathi97@gmail.com>
2024-03-18 14:53:11 +0530

    Created UserList component to show all users

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from '@/app/components/sidebar/Sidebar'
+import getUsers from '@/app/actions/getUsers'
+import UserList from '@/app/users/components/UserList';
 
-const UserLayout = ({ children }: { children: React.ReactNode }) => {
+const UserLayout = async ({ children }: { children: React.ReactNode }) => {
+    const users = await getUsers();
     return (
         <Sidebar>
             <div className='h-full'>
+                <UserList items={users} />
                 {children}
             </div>
         </Sidebar>
