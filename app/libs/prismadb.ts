839327324e26b89a8606c35dd3416fc8957f9340
Pratap Rathi <prataprathi97@gmail.com>
2024-03-19 22:52:44 +0530

    Updated Prisma client file

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -6,6 +6,6 @@ declare global{
 
 const client = globalThis.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV === "production") globalThis.prisma = client;
+if(process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
 export default client;
